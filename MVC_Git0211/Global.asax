64c6de7a09aed533b0179152002205b2772c6c5d
851030567@qq.com <851030567@qq.com>
2019-05-30 19:55:23 +0800

    添加项目文件。

---
+++ MVC_Git0211/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Git0211.MvcApplication" Language="C#" %>
---
+++ MVC_Git0211/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Git0211.Startup))]
+namespace MVC_Git0211
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
